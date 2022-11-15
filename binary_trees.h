6fc760070b4bc67edc9fcabefc55b8cbec535484
radar2 <securkydroity@gmail.com>
2022-11-15 08:43:57 -0800

    Fix Binary header

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +20,10 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BENARY_TREES_H */
