90754b82d28bdaa3199911f6bc301ac7b0ccb0aa
radar2 <securkydroity@gmail.com>
2022-11-15 11:39:39 -0800

    fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	node->n = value;
-	node-parent = parent;
+	node->parent = parent;
 
 	if (parent->right != NULL)
 		parent->right->parent = node;
