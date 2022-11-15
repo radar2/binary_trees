8965da27cb9b35ebd232cc3188a3b8af0c9612f7
radar2 <securkydroity@gmail.com>
2022-11-15 11:21:16 -0800

    Fix left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,31 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to node to insert
- * @value: value to put in node
- * Return: pointer to new node or NULL
+ * binary_tree_insert_left - function that inserts a node as the left-child
+ * @parent: a pointer to the node to insert the left-child in
+ * @value: the value to store in the new node
+ * Return: a pointer to the created node, or NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = NULL;
+	binary_tree_t *node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
+	node->n = value;
+	node->parent = parent;
 
 	if (parent->left != NULL)
-	{
-		parent->left->parent = new
-		new->left = parent->left;
-	}
+		parent->left->parent = node;
 
-	parent->left = new;
+	node->left = parent->left;
+	node->right = NULL;
+	parent->left = node;
 
-	return (new);
+	return (NULL);
 }
